f5fe8f9d464a9482fe5ee62cd1f3f03cdc72bf33
Endika (HTC) <endika2@gmail.com>
2015-02-06 12:07:39 +0100

    add less friendship app

--- weertik/friends/models.py
+++ weertik/friends/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class friendship(models.Model):
+    STATUS_LIST = ((u'W', u'Wait'),
+                   (u'A', u'Accept'),
+                   (u'B', u'Blocked'),
+                   )
+    from_user = models.ForeignKey(User,
+                                  blank=False)
+    to_user = models.ForeignKey(User,
+                                blank=False)
+    status = models.CharField('Status',
+                              blank=False,
+                              choizes=STATUS_LIST)
+    date = models.DateTimeField(auto_now_add=True,
+                                blank=True)
--- weertik/friends/views.py
+++ weertik/friends/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import *
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+@login_required(login_url='/')
+class friendships(request):
+    user = request.user
+    my_friends = Friendship.objects.get(from_user=user, status='A')
+    my_friends += Friendship.objects.get(to_user=user, status='A')
+    render_to_response('friends.html',
+                       {'friends': my_friends},
+                       context_instance=RequestContext(request))
